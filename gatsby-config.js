ea016383ef30f1c88518349d3faa1a011487e700
efand <andtheron@gmail.com>
2020-10-18 06:31:27 +0700

    try to deploy on GitHub

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@
 
 module.exports = {
   /* Your site config here */
+  pathPrefix: "/blog",
   siteMetadata: require("./site-meta-data.json"),
   plugins: [
     {
@@ -37,15 +38,15 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it. replace with yours
-        trackingId: "UA-164743872-1",
+        trackingId: "",
         head: true,
       }
     },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Delog GatbsyJS Starter`,
-        short_name: `Delog`,
+        name: `Yosef Blog`,
+        short_name: `Blog`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#381696`,
